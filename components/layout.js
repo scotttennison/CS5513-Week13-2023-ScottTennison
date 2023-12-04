3d6515eb37a097e8703e2fd2080229ff27c61b04
Scott Tennison <105877930+scotttennison@users.noreply.github.com>
2023-12-03 18:32:37 -0800

    Add files via upload

---
+++ components/layout.js
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function Layout({ children, home }) {
+  return (
+    <div className="layout-container">
+      <Head>
+        <title>Assignment 13</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        />
+      </Head>
+      
+
+      <main className="main-content">
+        {children}
+      </main>
+      {!home && (
+        <Link href="/" className="btn text-white mt-3 back-to-home">
+          ← Back to home
+        </Link>
+      )}
+
+   
+
+    </div>
+  );
+}
